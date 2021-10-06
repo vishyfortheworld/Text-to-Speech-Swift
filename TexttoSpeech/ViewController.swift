921338734691cd963d38d8d318a16d3c7f49133d
Vishrut Vatsa <vishrut.official@gmail.com>
2021-10-06 18:00:39 +0530

    Initial Commit

---
+++ TexttoSpeech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TexttoSpeech
+//
+//  Created by Vishrut Vatsa on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
