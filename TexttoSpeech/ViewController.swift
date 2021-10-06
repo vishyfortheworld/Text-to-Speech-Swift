02dd4db8bb1fb89103f08479262919fa1b3ddb98
Vishrut Vatsa <vishrut.official@gmail.com>
2021-10-06 19:45:32 +0530

    Just a few basics. Has errors too unfortunately but gotta have dinner.

--- TexttoSpeech/ViewController.swift
+++ TexttoSpeech/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// All Outlets
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var speechButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTappedAction(_ sender: Any) {
+        
+        if sender == clearButton {
+            //clear button tapped
+            textView.text = ""
+            
+            
+        }
+        
+        else if sender == speechButton {
+            // speech button tapped
+            
+        }
+    }
+    
+    
 }
 
